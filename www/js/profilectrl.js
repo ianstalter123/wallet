9d383f1a66a090e0e97bea77e800ed37001926fa
ianstalter123 <istalter66@gmail.com>
2016-04-06 19:57:13 -0700

    update

--- www/js/profilectrl.js
+++ www/js/profilectrl.js
@@ -1,9 +1,8 @@
 app.controller('profileCtrl', function($scope,$rootScope,$firebaseAuth,FirebaseConfig,$firebaseObject,authService) {
 
-  var authData = authService.authData;
 
   var ref = new Firebase(FirebaseConfig.base + "/wallets")
-  ref.orderByChild("uid").equalTo(authData.uid).on("child_added", function(snapshot) {
+  ref.orderByChild("uid").equalTo(authService.authData.uid).on("child_added", function(snapshot) {
     $scope.key = snapshot.key();
     console.log($scope.key);
     var walRef = new Firebase(FirebaseConfig.base + "/wallets/" +  snapshot.key())
